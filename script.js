da52cfb3de4b362329dbbfda41d113053861fca6
Andres Agudelo <anagudelogu@gmail.com>
2022-04-13 12:10:19 -0500

    Change the structure of the artist object
    Changed the type of 'photo' property  of the artist object from array to string.

--- script.js
+++ script.js
@@ -25,44 +25,43 @@ menuLinks.forEach((link) => link.addEventListener('click', linkClicked));
 // Dynamically generate the artists cards.
 
 const artists = [
-    {
-        name: 'Black Coffee',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    },
-    {
-        name: 'Amelie Lens',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    },
-    {
-        name: 'Richie Hawtin',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    },
-    {
-        name: 'Carl Cox',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    },
-    {
-        name: 'Anastasia Kristen',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    },
-    {
-        name: 'Boris Brejcha',
-        style: 'Tech-house beats',
-        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
-        photo: ['./images/artist01-bc.png', 'Black Coffee photo']
-    }
-]
-
+  {
+    name: 'Black Coffee',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+  {
+    name: 'Amelie Lens',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+  {
+    name: 'Richie Hawtin',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+  {
+    name: 'Carl Cox',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+  {
+    name: 'Anastasia Kristen',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+  {
+    name: 'Boris Brejcha',
+    style: 'Tech-house beats',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+    photo: './images/artist01-bc.png',
+  },
+];
 
 const cardsContainer = document.querySelector('.artist__cards');
 const article = document.createElement('article');
@@ -71,32 +70,28 @@ const div = document.createElement('div');
 const h3 = document.createElement('h3');
 const p = document.createElement('p');
 
+function createCard(artist) {
+  const artistCard = article.cloneNode(true);
+  const artistPhoto = img.cloneNode(true);
+  const artistInfo = div.cloneNode(true);
+  const artistName = h3.cloneNode(true);
+  const artistStyle = p.cloneNode(true);
+  const artistDesc = p.cloneNode(true);
 
-function createCard(artist){
-    const artistCard = article.cloneNode(true);
-    const artistPhoto = img.cloneNode(true);
-    const artistInfo = div.cloneNode(true);
-    const artistName = h3.cloneNode(true);
-    const artistStyle = p.cloneNode(true);
-    const artistDesc = p.cloneNode(true);
-
-    cardsContainer.append(artistCard);
-    artistCard.classList.add('artist__card');
-    artistCard.append(artistPhoto, artistInfo);
-    artistPhoto.classList.add('artist__image');
-    artistPhoto.src = artist.photo[0];
-    artistPhoto.setAttribute('alt', artist.photo[1]);
-    artistInfo.classList.add('artist__info');
-    artistInfo.append(artistName, artistStyle, artistDesc);
-    artistName.classList.add('artist__name');
-    artistName.innerText = artist.name;
-    artistStyle.classList.add('artist__style');
-    artistStyle.innerText = artist.style;
-    artistDesc.classList.add('artist__desc');
-    artistDesc.innerText = artist.desc;
+  cardsContainer.append(artistCard);
+  artistCard.classList.add('artist__card');
+  artistCard.append(artistPhoto, artistInfo);
+  artistPhoto.classList.add('artist__image');
+  artistPhoto.src = artist.photo;
+  artistPhoto.setAttribute('alt', `${artist.name} Photo`);
+  artistInfo.classList.add('artist__info');
+  artistInfo.append(artistName, artistStyle, artistDesc);
+  artistName.classList.add('artist__name');
+  artistName.innerText = artist.name;
+  artistStyle.classList.add('artist__style');
+  artistStyle.innerText = artist.style;
+  artistDesc.classList.add('artist__desc');
+  artistDesc.innerText = artist.desc;
 }
 
-artists.forEach(artist => createCard(artist));
-
-
-
+artists.forEach((artist) => createCard(artist));
